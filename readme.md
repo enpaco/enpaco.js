0b4603dabdf875e7620a54a8a0f78012913c0e8d
Daniel Supernault <danielsupernault@gmail.com>
2016-06-20 21:51:03 -0600

    Add initial enpaco implementation

---
+++ readme.md
@@ -0,0 +1,1 @@
+## enpaco.js
---
+++ src/enpaco.js
@@ -0,0 +1,35 @@
+(function () {
+  var enpaco = {};
+
+  var encrypt = enpaco.encrypt = function(message) {
+    var msg = nacl.util.decodeUTF8(message);
+    var key = nacl.randomBytes(nacl.secretbox.keyLength);
+    var nonce = nacl.randomBytes(nacl.secretbox.nonceLength);
+    var box = nacl.secretbox(msg, nonce, key);
+
+    var payload = new Uint8Array(nonce.byteLength + box.byteLength);
+    payload.set(nonce, 0);
+    payload.set(box, nonce.byteLength);
+
+    var ascii_payload = nacl.util.encodeBase64(payload);
+    var ascii_key = nacl.util.encodeBase64(key);
+
+    return {
+        payload: ascii_payload,
+        key: ascii_key
+    }
+  }
+
+  var decrypt = enpaco.decrypt = function(ascii_payload, ascii_key) {
+    var key = nacl.util.decodeBase64(ascii_key);
+    var payload = nacl.util.decodeBase64(ascii_payload);
+    var nonce = payload.slice(0, nacl.secretbox.nonceLength);
+    var box = payload.slice(nacl.secretbox.nonceLength);
+
+    var message = nacl.secretbox.open(box, nonce, key);
+    message = nacl.util.encodeUTF8(message);
+
+    return message;
+}
+window.enpaco = enpaco;
+}());
